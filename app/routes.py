d5b7d6242022eef9c967f03b71a16a866b503714
I528959 <antony.berlin.r@sap.com>
2024-11-20 21:00:12 +0530

    funciton to extract text from the file

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, jsonify
-
+from flask import Blueprint, jsonify, request
+from utils import extract_text
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
     return jsonify({"message": "hello mic check 1, 2, 3"})
 
+@main.route("/extract_text", methods=["POST"])
+def get_text_from_file():
+   data = request.json()
+   file_name  = data["file_name"]
+   text  = extract_text(file_name)
+   return jsonify({"text": text}),200
--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,30 @@
 #helper function here
+import PyPDF2
+from pptx import Presentation
+import os
+
+def extract_text(file):
+    file_extension = os.path.splitext(file)[1].lower()
+    
+    if file_extension == '.ppt' or file_extension == '.pptx':
+        prs = Presentation(file)
+        text_runs = []
+        for slide in prs.slides:
+            for shape in slide.shapes:
+                if hasattr(shape, "text"):
+                    text_runs.append(shape.text)
+                    text_runs.append("-"*10)
+        return "\n".join(text_runs)
+    
+    elif file_extension == '.pdf':
+        text_runs = []
+        with open(file, 'rb') as pdf_file:
+            reader = PyPDF2.PdfFileReader(pdf_file)
+            for page_num in range(reader.numPages):
+                page = reader.getPage(page_num)
+                text_runs.append(page.extract_text())
+                text_runs.append("-"*10)
+        return "\n".join(text_runs)
+    
+    else:
+        raise ValueError("Unsupported file format. Please provide a PPT, PPTX, or PDF file.")
